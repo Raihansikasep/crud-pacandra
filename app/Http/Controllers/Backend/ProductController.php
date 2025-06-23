17eddc7c0ac8597f88d093d71bf6151b1aea472c
Raihansikasep <raiifirdauss@gmail.com>
2025-06-23 20:38:32 +0700

    bagian product

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -3,83 +3,113 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Storage;
+use Str;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $product = Product::latest()->get();
+
+        $title = 'Hapus Data!';
+        $text = "Apakah anda yakin?";
+        confirmDelete($title, $text);
+
+        return view('backend.product.index', compact('product'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        $category = Category::all(); // untuk select option
+        return view('backend.product.create', compact('category'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name'        => 'required|unique:categories',
+            'category_id' => 'required',
+            'price'       => 'required|numeric',
+            'description' => 'required',
+            'stock'       => 'required|numeric',
+            'image'       => 'required|image|mimes:jpg,png',
+        ]);
+
+        $product = new Product();
+        $product->name        = $request->name;
+        $product->slug        = Str::slug($request->name, '-');
+        $product->category_id = $request->category_id;
+        $product->description = $request->description;
+        $product->price       = $request->price;
+        $product->stock       = $request->stock;
+
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $randomName = Str::random(20) . '.' . $file->getClientOriginalExtension();
+            $path = $file->storeAs('products', $randomName, 'public');
+            $product->image = $path;
+        }
+
+        $product->save();
+        toast('Data berhasil disimpan', 'success');
+        return redirect()->route('product.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        return view('backend.product.show', compact('product'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function edit(string $id)
     {
-        //
+        $product  = Product::findOrFail($id);
+        $category = Category::all();
+        return view('backend.product.edit', compact('product', 'category'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(Request $request, string $id)
     {
-        //
+        $validated = $request->validate([
+            'name'        => 'required|unique:categories',
+            'category_id' => 'required',
+            'price'       => 'required|numeric',
+            'description' => 'required',
+            'stock'       => 'required|numeric',
+            // 'image'       => 'image|mimes:jpg,png',
+        ]);
+
+        $product = Product::findOrFail($id);
+        $product->name        = $request->name;
+        $product->slug        = Str::slug($request->name, '-');
+        $product->category_id = $request->category_id;
+        $product->description = $request->description;
+        $product->price       = $request->price;
+        $product->stock       = $request->stock;
+
+        if ($request->hasFile('image')) {
+            Storage::disk('public')->delete($product->image);
+            $file = $request->file('image');
+            $randomName = Str::random(20) . '.' . $file->getClientOriginalExtension();
+            $path = $file->storeAs('products', $randomName, 'public');
+            $product->image = $path;
+        }
+
+        $product->save();
+        toast('Data berhasil disimpan', 'success');
+        return redirect()->route('product.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function destroy(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        Storage::disk('public')->delete($product->image);
+        $product->delete();
+        toast('Data berhasil dihapus', 'success');
+        return redirect()->route('product.index');
     }
 }
---
+++ resources/views/backend/product/edit.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.backend')
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">Edit Produk</div>
+                <div class="card-body">
+                    <form action="{{ route('product.update',$product->id) }}" method="post" enctype="multipart/form-data">
+                        @method('PUT')
+                        @csrf
+                        <div class="row">
+                            <div class="col">
+                                <div class="mb-2">
+                                    <label>Nama Product</label>
+                                    <input type="text" value="{{ $product->name }}" name="name" class="form-control @error('name') is-invalid @enderror">
+                                    @error('name')<span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>@enderror
+                                </div>
+
+                                <div class="mb-2">
+                                    <label>Harga</label>
+                                    <input type="number" value="{{ $product->price }}" name="price" class="form-control @error('price') is-invalid @enderror">
+                                    @error('price')<span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>@enderror
+                                </div>
+
+                                <div class="mb-2">
+                                    <label>Kategori</label>
+                                    <select name="category_id" class="form-control @error('category_id') is-invalid @enderror">
+                                        @foreach ($category as $data)
+                                        <option value="{{ $data->id }}" {{ $data->id == $product->category_id ? 'selected' : '' }}>{{ $data->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    @error('category_id')<span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>@enderror
+                                </div>
+
+                                <div class="mb-2">
+                                    <label>Stok</label>
+                                    <input type="number" value="{{ $product->stock }}" name="stock" class="form-control @error('stock') is-invalid @enderror">
+                                    @error('stock')<span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>@enderror
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                @if($product->image)
+                                <img src="{{ Storage::url($product->image) }}" style="width: 100px; height:100px;">
+                                @endif
+
+                                <div class="mb-2">
+                                    <label>Gambar</label>
+                                    <input type="file" name="image" class="form-control @error('image') is-invalid @enderror">
+                                    @error('image')<span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>@enderror
+                                </div>
+
+                                <div class="mb-2">
+                                    <label>Deskripsi</label>
+                                    <textarea name="description" class="form-control @error('description') is-invalid @enderror">{{ $product->description }}</textarea>
+                                    @error('description')<span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>@enderror
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-sm btn-outline-primary">Simpan</button>
+                        <button type="reset" class="btn btn-sm btn-outline-warning">Reset</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
